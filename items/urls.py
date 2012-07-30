bface4ec3c558b674cb5b5be146c92909c7cddf0
Daniel St. Jules <danielst.jules@gmail.com>
2012-07-30 11:46:59 -0400

    re-organize structure

--- items/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('items.views',
-    url(r'^$', 'index'),
-    url(r'^(?P<item_id>\d+)/$', 'detail'),
-    url(r'^(?P<item_id>\d+)/request/$', 'request'),
-)
