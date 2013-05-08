bcaaaff1694ac2d66da0acd0136978b903e7261e
Daniel St. Jules <danielst.jules@gmail.com>
2013-05-08 19:47:02 -0400

    Use root for routing labtracker requests

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^labtracker/', include('labtracker.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^', include('labtracker.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
